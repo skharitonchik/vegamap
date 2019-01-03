395ae28fbefc9fdab0d74cce194d127bf74ff893
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2019-01-03 17:10:37 +0300

    add few markers

--- markers.js
+++ markers.js
@@ -1,11 +1,19 @@
 /**
  * latitude[lat] - широта
  * longitude[lon] - долгота
+ name: '',
+ geo: [{
+        latitude: 53,
+        longitude: 27,
+        address: '',
+        timeOfWork: '',
+        phones: [''],
+    }],
+ links: ['']
  * @type {*[]}
  */
 let MARKERS = [{
     name: 'Супкультура',
-    address: 'г. Минск, ул. Козлова, 5',
     geo: [{
         latitude: 53.90956,
         longitude: 27.5807,
@@ -19,17 +27,60 @@ let MARKERS = [{
         timeOfWork: '10.00-20.00',
         phones: ['+375 (29) 176-01-01'],
     }],
-    links: ['https://www.instagram.com/soupculture.minsk/']
+    links: ['https://www.instagram.com/soupculture.minsk/',
+        'https://www.facebook.com/soupculture.by',
+        'https://vk.com/soupcultureby']
 }, {
-    name: 'Vegaburger',
+    name: 'VegaBurger',
     geo: [{
         latitude: 53.90193,
-        longitude: 27.56125
-    }]
+        longitude: 27.56125,
+        address: 'Октябрьская площадь, 2 (подземный переход)',
+        timeOfWork: '11:00–19:00',
+        phones: ['+375 (29) 649-17-86'],
+    }],
+    links: ['https://www.instagram.com/vegaburger']
 }, {
     name: 'Monkey Food',
     geo: [{
         latitude: 53.93646,
-        longitude: 27.58462
-    }]
+        longitude: 27.58462,
+        address: 'ул. Богдановича, 128а',
+        timeOfWork: '11:00–22:00',
+        phones: ['+375 (33) 352-06-06', '+375 (29) 352-06-06'],
+    }],
+    links: ['https://www.instagram.com/monkey.food']
+}, {
+    name: 'OM NAMO',
+    geo: [{
+        latitude: 53.91950,
+        longitude: 27.57444,
+        address: 'ул. В.Хоружей, 13',
+        timeOfWork: '11.00-23.00',
+        phones: ['+375 (44) 764-78-86'],
+    }],
+    links: ['https://omnamo.by/',
+        'https://www.instagram.com/bistroomnamo/',
+        'https://www.facebook.com/bistroomnamo',
+        'https://vk.com/bistroomnamo']
+}, {
+    name: 'Лавка «Живой край»',
+    geo: [{
+        latitude: 53.94272,
+        longitude: 27.59756,
+        address: 'улица Леонида Беды, 45',
+        timeOfWork: '10:00–20:00',
+        phones: ['+375 (29) 258-75-83'],
+    }],
+    links: ['https://www.instagram.com/zhivoikrai/']
+}, {
+    name: 'VEGGIE BAR',
+    geo: [{
+        latitude: 53.87465,
+        longitude: 27.62756,
+        address: 'проспект Партизанский, 52 (внутри «Чайного домика Будды»)',
+        timeOfWork: '12:00–20:00',
+        phones: ['+375 (29) 123-13-21'],
+    }],
+    links: ['https://vk.com/veggbar']
 }];
