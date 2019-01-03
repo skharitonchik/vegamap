ae92cc9235f584d1f713a45c0848a8c712eee011
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2019-01-03 11:56:31 +0300

    add markers base

---
+++ markers.js
@@ -0,0 +1,30 @@
+/**
+ * latitude[lat] - широта
+ * longitude[lon] - долгота
+ * @type {*[]}
+ */
+let MARKERS = [{
+    name: 'Супкультура',
+    address: 'г. Минск, ул. Козлова, 5',
+    geo: [{
+        latitude: 53.90956,
+        longitude: 27.5807
+    }, {
+        latitude: 53.86144,
+        longitude: 27.47989
+    }],
+    timeOfWork: '11:00 — 21:00',
+    phones: ['+375(29) 175-01-01']
+}, {
+    name: 'Vegaburger',
+    geo: [{
+        latitude: 53.90193,
+        longitude: 27.56125
+    }]
+}, {
+    name: 'Monkey Food',
+    geo: [{
+        latitude: 53.93646,
+        longitude: 27.58462
+    }]
+}];
