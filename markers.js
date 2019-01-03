015a9fd983cc2586ca85bec6e4041cf44ae1586d
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2019-01-03 16:39:58 +0300

    add more info for geo places

--- markers.js
+++ markers.js
@@ -8,13 +8,18 @@ let MARKERS = [{
     address: 'г. Минск, ул. Козлова, 5',
     geo: [{
         latitude: 53.90956,
-        longitude: 27.5807
+        longitude: 27.5807,
+        address: 'ул. Козлова, 5',
+        timeOfWork: '11.00-21.00',
+        phones: ['+375 (29) 175-01-01'],
     }, {
         latitude: 53.86144,
-        longitude: 27.47989
+        longitude: 27.47989,
+        address: 'Пр-т Дзержинского, 104',
+        timeOfWork: '10.00-20.00',
+        phones: ['+375 (29) 176-01-01'],
     }],
-    timeOfWork: '11:00 — 21:00',
-    phones: ['+375(29) 175-01-01']
+    links: ['https://www.instagram.com/soupculture.minsk/']
 }, {
     name: 'Vegaburger',
     geo: [{
