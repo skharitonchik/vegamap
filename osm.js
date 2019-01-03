a3720b38fdf164b3a6054537c1a904e4f304a6c7
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2019-01-03 15:38:10 +0300

    move implementation to leaflet 1.4.0

--- osm.js
+++ osm.js
@@ -1,43 +1,26 @@
-function init() {
-    let map = new OpenLayers.Map('vegamap'),
-        markers = new OpenLayers.Layer.Markers("Markers"),
-        zoom = 12;
-
-    const from = new OpenLayers.Projection("EPSG:4326"),
-        to = map.getProjectionObject();
-
-    map.addLayer(new OpenLayers.Layer.OSM());
-
-    console.info(getMarkers(map), 'SERGEY getMarkers(map)');
-    getMarkers(map).forEach(m => markers.addMarker(m));
-
-    map.addLayer(markers);
-
-    let minskCenter = new OpenLayers.LonLat(27.55972, 53.90138)
-        .transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-            map.getProjectionObject() // to Spherical Mercator Projection
-        );
-
-    map.setCenter(minskCenter, zoom);
+function placeMarkers(map) {
+    MARKERS.forEach(m => {
+        if (m.geo && m.geo.length > 0) {
+            m.geo.forEach(g => {
+                L.marker([g.latitude, g.longitude]).addTo(map)
+                    .bindPopup(`${m.name}<br>${m.address || ''} <br> ${m.timeOfWork || ''} <br> ${m.phones || ''}`);
+            });
+        }
+    });
 }
 
-function getMarkers(map) {
-    let markers = [];
-    const from = new OpenLayers.Projection("EPSG:4326"),
-        to = map.getProjectionObject();
+function init() {
+    const MINSK_CENTER = {
+        latitude: 53.90138,
+        longitude: 27.55972
+    };
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const MAP_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-    MARKERS.forEach(m => {
-        m.geo.forEach(g => {
-            const markerPos = new OpenLayers
-                .LonLat(g.longitude, g.latitude)
-                .transform(from, to);
+    let map = L.map('vegamap').setView([MINSK_CENTER.latitude, MINSK_CENTER.longitude], 12);
 
-            markers.push(new OpenLayers.Marker(markerPos));
-        });
-    });
-
-    return markers;
+    L.tileLayer(MAP_LAYER, {attribution}).addTo(map);
+    placeMarkers(map);
 }
 
 document.addEventListener('DOMContentLoaded', () => init());
