ae176e34dcdefe0957c675c943d944bd591b9bd4
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2019-01-03 16:43:38 +0300

    handle markers, add popup text handler

--- osm.js
+++ osm.js
@@ -1,12 +1,36 @@
 function placeMarkers(map) {
     MARKERS.forEach(m => {
         if (m.geo && m.geo.length > 0) {
-            m.geo.forEach(g => {
-                L.marker([g.latitude, g.longitude]).addTo(map)
-                    .bindPopup(`${m.name}<br>${m.address || ''} <br> ${m.timeOfWork || ''} <br> ${m.phones || ''}`);
-            });
+            m.geo.forEach(g => L.marker([g.latitude, g.longitude]).addTo(map).bindPopup(popupText(g, m)));
         }
     });
+}
+
+function popupText(currentGeo, currentMarker) {
+    let baseText = `${currentMarker.name} <br>`;
+
+    try {
+        if (currentGeo.address) {
+            baseText += `${currentGeo.address}<br>`;
+        }
+
+        if (currentGeo.timeOfWork) {
+            baseText += `${currentGeo.timeOfWork}<br>`;
+        }
+
+        if (currentGeo.phones && currentGeo.phones.length > 0) {
+            currentGeo.phones.forEach(p => baseText += `${p}  `);
+            baseText += '<br>';
+        }
+
+        if (currentMarker.links && currentMarker.links.length > 0) {
+            currentMarker.links.forEach(l => baseText += `<a href="${l}">${l}</a><br>`);
+        }
+    } catch (e) {
+        //silent
+    }
+
+    return baseText;
 }
 
 function init() {
@@ -14,12 +38,16 @@ function init() {
         latitude: 53.90138,
         longitude: 27.55972
     };
-    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-    const MAP_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const MAP_ID = 'vegamap';
 
-    let map = L.map('vegamap').setView([MINSK_CENTER.latitude, MINSK_CENTER.longitude], 12);
+    const MAP_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+
+    let map = L.map(MAP_ID).setView([MINSK_CENTER.latitude, MINSK_CENTER.longitude], 12);
 
     L.tileLayer(MAP_LAYER, {attribution}).addTo(map);
+
     placeMarkers(map);
 }
 
