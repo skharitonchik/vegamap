26480820d26f973bcc0c92fc6dd0acf11e46f5d9
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2019-06-07 18:52:14 +0300

    update marker for SupCulture, add Yellow slon

--- scripts/markers.js
+++ scripts/markers.js
@@ -21,12 +21,6 @@ let MARKERS = [{
         address: 'ул. Козлова, 5',
         timeOfWork: '11.00-21.00',
         phones: ['+375 (29) 175-01-01'],
-    }, {
-        latitude: 53.86144,
-        longitude: 27.47989,
-        address: 'Пр-т Дзержинского, 104',
-        timeOfWork: '10.00-20.00',
-        phones: ['+375 (29) 176-01-01'],
     }],
     links: [
         'https://www.instagram.com/soupculture.minsk/',
@@ -34,91 +28,105 @@ let MARKERS = [{
         'https://vk.com/soupcultureby'
     ]
 }, {
-    name: 'VegaBurger',
+    name: 'Yellow SLON',
     geo: [{
-        latitude: 53.90193,
-        longitude: 27.56125,
-        address: 'Октябрьская площадь, 2 (подземный переход)',
-        timeOfWork: '11:00–19:00',
-        phones: ['+375 (29) 649-17-86'],
+        latitude: 53.92333,
+        longitude: 27.56875,
+        address: 'ул. Богдановича, 133',
+        timeOfWork: 'ПН-ПТ 11:22.00 СБ-ВС 12:22:00',
+        phones: ['+375 (29) 113-85-13'],
     }],
     links: [
-        'https://www.instagram.com/vegaburger',
-        'https://vk.com/vegaburger',
-        'https://www.facebook.com/VegaBurgerMinsk'
+        'https://www.yellowslon.by',
+        'https://vk.com/yellowslon'
     ]
-}, {
-    name: 'Monkey Food',
-    geo: [{
-        latitude: 53.93646,
-        longitude: 27.58462,
-        address: 'ул. Богдановича, 128а',
-        timeOfWork: '11:00–22:00',
-        phones: ['+375 (29) 352-06-06'],
+},
+    {
+        name: 'VegaBurger',
+        geo: [{
+            latitude: 53.90193,
+            longitude: 27.56125,
+            address: 'Октябрьская площадь, 2 (подземный переход)',
+            timeOfWork: '11:00–19:00',
+            phones: ['+375 (29) 649-17-86'],
+        }],
+        links: [
+            'https://www.instagram.com/vegaburger',
+            'https://vk.com/vegaburger',
+            'https://www.facebook.com/VegaBurgerMinsk'
+        ]
     }, {
+        name: 'Monkey Food',
+        geo: [{
+            latitude: 53.93646,
+            longitude: 27.58462,
+            address: 'ул. Богдановича, 128а',
+            timeOfWork: '11:00–22:00',
+            phones: ['+375 (29) 352-06-06'],
+        }, {
             latitude: 53.88977,
             longitude: 27.57446,
             address: 'ул. Октябрьская, 16',
             timeOfWork: '11:00–22:00',
             phones: ['+375 (29) 352-06-06'],
-    }],
-    links: [
-        'http://monkeyfood.by',
-        'https://www.instagram.com/monkey.food',
-        'https://vk.com/monkeyfood',
-        'https://www.facebook.com/groups/MonkeyFood'
-    ]
-}, {
-    name: 'OM NAMO',
-    geo: [{
-        latitude: 53.91950,
-        longitude: 27.57444,
-        address: 'ул. В.Хоружей, 13',
-        timeOfWork: '11.00-23.00',
-        phones: ['+375 (44) 764-78-86'],
-    }],
-    links: [
-        'https://omnamo.by/',
-        'https://www.instagram.com/bistroomnamo/',
-        'https://www.facebook.com/bistroomnamo',
-        'https://vk.com/bistroomnamo']
-}, {
-    name: 'Лавка «Живой край»',
-    geo: [{
-        latitude: 53.94272,
-        longitude: 27.59756,
-        address: 'улица Леонида Беды, 45',
-        timeOfWork: '10:00–20:00',
-        phones: ['+375 (29) 258-75-83'],
-    }],
-    links: [
-        'https://www.instagram.com/zhivoikrai/',
-        'https://vk.com/zhivoi_krai'
-    ]
-}, {
-    name: 'VEGGIE BAR',
-    geo: [{
-        latitude: 53.87465,
-        longitude: 27.62756,
-        address: 'проспект Партизанский, 52 (внутри «Чайного домика Будды»)',
-        timeOfWork: '12:00–20:00',
-        phones: ['+375 (29) 123-13-21'],
-    }],
-    links: ['https://vk.com/veggbar']
-}, {
-    name: 'Vegan Pizza Minsk',
-    geo: [{
-        latitude: 53.9196,
-        longitude: 27.57369,
-        address: 'улица Веры Хоружей, 15',
-        timeOfWork: 'Ср-Вс: 15:00-21:00 Пн-Вт: выходной',
-        phones: ['+375 (29) 878-11-87'],
-    }],
-    links: [
-        'https://vegan-pizza-minsk.business.site',
-        'https://www.instagram.com/vegan.pizza.minsk'
-    ]
-}];
+        }],
+        links: [
+            'http://monkeyfood.by',
+            'https://www.instagram.com/monkey.food',
+            'https://vk.com/monkeyfood',
+            'https://www.facebook.com/groups/MonkeyFood'
+        ]
+    }, {
+        name: 'OM NAMO',
+        geo: [{
+            latitude: 53.91950,
+            longitude: 27.57444,
+            address: 'ул. В.Хоружей, 13',
+            timeOfWork: '11.00-23.00',
+            phones: ['+375 (44) 764-78-86'],
+        }],
+        links: [
+            'https://omnamo.by/',
+            'https://www.instagram.com/bistroomnamo/',
+            'https://www.facebook.com/bistroomnamo',
+            'https://vk.com/bistroomnamo']
+    }, {
+        name: 'Лавка «Живой край»',
+        geo: [{
+            latitude: 53.94272,
+            longitude: 27.59756,
+            address: 'улица Леонида Беды, 45',
+            timeOfWork: '10:00–20:00',
+            phones: ['+375 (29) 258-75-83'],
+        }],
+        links: [
+            'https://www.instagram.com/zhivoikrai/',
+            'https://vk.com/zhivoi_krai'
+        ]
+    }, {
+        name: 'VEGGIE BAR',
+        geo: [{
+            latitude: 53.87465,
+            longitude: 27.62756,
+            address: 'проспект Партизанский, 52 (внутри «Чайного домика Будды»)',
+            timeOfWork: '12:00–20:00',
+            phones: ['+375 (29) 123-13-21'],
+        }],
+        links: ['https://vk.com/veggbar']
+    }, {
+        name: 'Vegan Pizza Minsk',
+        geo: [{
+            latitude: 53.9196,
+            longitude: 27.57369,
+            address: 'улица Веры Хоружей, 15',
+            timeOfWork: 'Ср-Вс: 15:00-21:00 Пн-Вт: выходной',
+            phones: ['+375 (29) 878-11-87'],
+        }],
+        links: [
+            'https://vegan-pizza-minsk.business.site',
+            'https://www.instagram.com/vegan.pizza.minsk'
+        ]
+    }];
 //endregion
 
 //region Vega shops
