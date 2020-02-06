5fc798bcfa253d386e8c50470bfe944fc78f01e9
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2020-02-06 19:37:12 +0300

    add Piligreen, update html text

--- scripts/markers.js
+++ scripts/markers.js
@@ -42,93 +42,88 @@ let MARKERS = [{
         'https://www.facebook.com/cafeyellowslonminsk/',
         'https://www.instagram.com/cafe_yellow_slon/'
     ]
-},
-    {
-        name: 'VegaBurger',
-        geo: [{
-            latitude: 53.90193,
-            longitude: 27.56125,
-            address: 'Октябрьская площадь, 2 (подземный переход)',
-            timeOfWork: '11:00–19:00',
-            phones: ['+375 (29) 649-17-86'],
-        }],
-        links: [
-            'https://www.instagram.com/vegaburger',
-            'https://vk.com/vegaburger',
-            'https://www.facebook.com/VegaBurgerMinsk'
-        ]
+}, {
+    name: 'Monkey Food',
+    geo: [{
+        latitude: 53.93646,
+        longitude: 27.58462,
+        address: 'ул. Богдановича, 128а',
+        timeOfWork: '11:00–22:00',
+        phones: ['+375 (29) 352-06-06'],
     }, {
-        name: 'Monkey Food',
-        geo: [{
-            latitude: 53.93646,
-            longitude: 27.58462,
-            address: 'ул. Богдановича, 128а',
-            timeOfWork: '11:00–22:00',
-            phones: ['+375 (29) 352-06-06'],
-        }, {
-            latitude: 53.88977,
-            longitude: 27.57446,
-            address: 'ул. Октябрьская, 16',
-            timeOfWork: '11:00–22:00',
-            phones: ['+375 (29) 352-06-06'],
-        }],
-        links: [
-            'http://monkeyfood.by',
-            'https://www.instagram.com/monkey.food',
-            'https://vk.com/monkeyfood',
-            'https://www.facebook.com/groups/MonkeyFood'
-        ]
-    }, {
-        name: 'OM NAMO',
-        geo: [{
-            latitude: 53.91950,
-            longitude: 27.57444,
-            address: 'ул. В.Хоружей, 13',
-            timeOfWork: '11.00-23.00',
-            phones: ['+375 (44) 764-78-86'],
-        }],
-        links: [
-            'https://omnamo.by/',
-            'https://www.instagram.com/bistroomnamo/',
-            'https://www.facebook.com/bistroomnamo',
-            'https://vk.com/bistroomnamo']
-    }, {
-        name: 'Лавка «Живой край»',
-        geo: [{
-            latitude: 53.94272,
-            longitude: 27.59756,
-            address: 'улица Леонида Беды, 45',
-            timeOfWork: '10:00–20:00',
-            phones: ['+375 (29) 258-75-83'],
-        }],
-        links: [
-            'https://www.instagram.com/zhivoikrai/',
-            'https://vk.com/zhivoi_krai'
-        ]
-    }, {
-        name: 'VEGGIE BAR',
-        geo: [{
-            latitude: 53.87465,
-            longitude: 27.62756,
-            address: 'проспект Партизанский, 52 (внутри «Чайного домика Будды»)',
-            timeOfWork: '12:00–20:00',
-            phones: ['+375 (29) 123-13-21'],
-        }],
-        links: ['https://vk.com/veggbar']
-    }, {
-        name: 'Vegan Pizza Minsk',
-        geo: [{
-            latitude: 53.9196,
-            longitude: 27.57369,
-            address: 'улица Веры Хоружей, 15',
-            timeOfWork: 'Ср-Вс: 15:00-21:00 Пн-Вт: выходной',
-            phones: ['+375 (29) 878-11-87'],
-        }],
-        links: [
-            'https://vegan-pizza-minsk.business.site',
-            'https://www.instagram.com/vegan.pizza.minsk'
-        ]
-    }];
+        latitude: 53.88977,
+        longitude: 27.57446,
+        address: 'ул. Октябрьская, 16',
+        timeOfWork: '11:00–22:00',
+        phones: ['+375 (29) 352-06-06'],
+    }],
+    links: [
+        'http://monkeyfood.by',
+        'https://www.instagram.com/monkey.food',
+        'https://vk.com/monkeyfood',
+        'https://www.facebook.com/groups/MonkeyFood'
+    ]
+}, {
+    name: 'OM NAMO',
+    geo: [{
+        latitude: 53.91950,
+        longitude: 27.57444,
+        address: 'ул. В.Хоружей, 13',
+        timeOfWork: '11.00-23.00',
+        phones: ['+375 (44) 764-78-86'],
+    }],
+    links: [
+        'https://omnamo.by/',
+        'https://www.instagram.com/bistroomnamo/',
+        'https://www.facebook.com/bistroomnamo',
+        'https://vk.com/bistroomnamo']
+}, {
+    name: 'Лавка «Живой край»',
+    geo: [{
+        latitude: 53.94272,
+        longitude: 27.59756,
+        address: 'улица Леонида Беды, 45',
+        timeOfWork: '10:00–20:00',
+        phones: ['+375 (29) 258-75-83'],
+    }],
+    links: [
+        'https://www.instagram.com/zhivoikrai/',
+        'https://vk.com/zhivoi_krai'
+    ]
+}, {
+    name: 'VEGGIE BAR',
+    geo: [{
+        latitude: 53.87465,
+        longitude: 27.62756,
+        address: 'проспект Партизанский, 52 (внутри «Чайного домика Будды»)',
+        timeOfWork: '12:00–20:00',
+        phones: ['+375 (29) 123-13-21'],
+    }],
+    links: ['https://vk.com/veggbar']
+}, {
+    name: 'Vegan Pizza Minsk',
+    geo: [{
+        latitude: 53.9196,
+        longitude: 27.57369,
+        address: 'улица Веры Хоружей, 15',
+        timeOfWork: 'Ср-Вс: 15:00-21:00 Пн-Вт: выходной',
+        phones: ['+375 (29) 878-11-87'],
+    }],
+    links: [
+        'https://vegan-pizza-minsk.business.site',
+        'https://www.instagram.com/vegan.pizza.minsk'
+    ]
+}, {
+    name: 'Pilligreen',
+    geo: [{
+        latitude: 53.93096,
+        longitude: 27.65360,
+        address: 'ул. Кирилла Туровского, 24 (цокольный этаж)',
+        timeOfWork: 'ПН-ВС 9:30-22:00',
+        phones: ['+375 (44) 758-70-80'],
+    }],
+    links: ['https://www.instagram.com/pilligreen.by', 'https://pilligreen.by/']
+}];
 //endregion
 
 //region Vega shops
